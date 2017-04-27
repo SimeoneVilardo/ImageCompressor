b87f250de979a5e38188845ba2c441206efba0ea
Simeone Vilardo <simeone.vilardo@gmail.com>
2017-04-27 20:30:28 +0200

    Add FileDirectoryDialog

--- ImageCompressorUI/ImageCompressorUI.Designer.cs
+++ ImageCompressorUI/ImageCompressorUI.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnBrowsePath = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(119, 29);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(413, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(35, 32);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(78, 13);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "File o directory:";
+            // 
+            // btnBrowsePath
+            // 
+            this.btnBrowsePath.Location = new System.Drawing.Point(538, 27);
+            this.btnBrowsePath.Name = "btnBrowsePath";
+            this.btnBrowsePath.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowsePath.TabIndex = 2;
+            this.btnBrowsePath.Text = "...";
+            this.btnBrowsePath.UseVisualStyleBackColor = true;
+            this.btnBrowsePath.Click += new System.EventHandler(this.btnBrowsePath_Click);
             // 
             // ImageCompressorUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(734, 261);
+            this.Controls.Add(this.btnBrowsePath);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.txtPath);
             this.Name = "ImageCompressorUI";
             this.Text = "ImageCompressor";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnBrowsePath;
     }
 }
 
